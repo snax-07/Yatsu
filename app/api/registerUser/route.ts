bfc5ecc8079bfefd45f53ca9aa5f4e8928e9ba0e
Swapnil Nade <141449289+snax-07@users.noreply.github.com>
2025-09-07 14:36:57 +0530

    Update route.ts

--- app/api/registerUser/route.ts
+++ app/api/registerUser/route.ts
@@ -1,3 +1,6 @@
+export const runtime = 'nodejs';
+
+
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import User from "@/model/userRegister.model";
